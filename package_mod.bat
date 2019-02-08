539c4826e3432ace83f92543cf65b6e6318e9bb5
CamDawg <pcamagna@yahoo.com>
2019-02-08 06:17:40 -0800

    Packaging update
    Bumped version number, corrected readme URL, added new tile2ee argument

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=The Calling"
-set mod_version=Beta_1
+set mod_version=Beta_2
 set mod_folder=thecalling
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
@@ -22,8 +22,9 @@ REM /* the mod_readme link should point to the online readme and work fine as-is
 set ico_folder=%mod_folder%\style
 set audio_folder=%mod_folder%\audio
 set tispack_folder=%mod_folder%\tiz
+set tile2ee_folder=%mod_folder%\tools\tile2ee
 set iconv_folder=%mod_folder%\languages\iconv
-set mod_readme=http://www.gibberlings3.net/readmes/readme-%mod_folder%.html
+set mod_readme=https://gibberlings3.github.io/Documentation/readmes/readme-%mod_folder%.html
 
 REM /* MODIFY: if you don't need to build a specific package, you can disable it by setting the appropriate variable below to 0 */
 set build_windows=1
